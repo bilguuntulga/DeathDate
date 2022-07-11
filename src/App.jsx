525f97d61d4009a7d3a8cbbd6e4219b2f5b5bf0e
Mn Monherdene <mnmonherdene1234@gmail.com>
2022-07-11 23:47:57 +0800

    Loading added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const Main = lazy(() => import('./components/Main'));
 const FrontEnd = lazy(() => import('./components/FrontEnd/FrontEnd'));
 const Flutter = lazy(() => import('./components/Flutter/Flutter'));
 const BackEnd = lazy(() => import('./components/BackEnd/BackEnd'));
+const Loading = lazy(() => import('./components/Loading'));
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
@@ -20,22 +21,22 @@ const App = () => {
             <Navigation />
             <Routes>
                 <Route exact path="/" element={
-                    <Suspense fallback="Loading...">
+                    <Suspense fallback={<Suspense><Loading /></Suspense>}>
                         <Main />
                     </Suspense>
                 } />
                 <Route exact path="front-end" element={
-                    <Suspense fallback="Loading...">
+                    <Suspense fallback={<Suspense><Loading /></Suspense>}>
                         <FrontEnd />
                     </Suspense>
                 } />
                 <Route exact path="flutter" element={
-                    <Suspense fallback="Loading...">
+                    <Suspense fallback={<Suspense><Loading /></Suspense>}>
                         <Flutter />
                     </Suspense>
                 } />
                 <Route exact path="back-end" element={
-                    <Suspense fallback="Loading...">
+                    <Suspense fallback={<Suspense><Loading /></Suspense>}>
                         <BackEnd />
                     </Suspense>
                 } />
